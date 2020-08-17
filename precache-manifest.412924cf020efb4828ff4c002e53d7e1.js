1973271be297d3f42e76c1e30f5a0c93394166fa
Momodou Bah <472mbah@gmail.com>
2020-08-17 19:30:06 +0100

    Updates

---
+++ precache-manifest.412924cf020efb4828ff4c002e53d7e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5ea91ad3f9ff290da5e3dfa6a511b2",
+    "url": "/chat337v2/index.html"
+  },
+  {
+    "revision": "9640ef3988c3f5166dfc",
+    "url": "/chat337v2/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "60142e6f9d74d00ddda7",
+    "url": "/chat337v2/static/css/main.61c31b1a.chunk.css"
+  },
+  {
+    "revision": "9640ef3988c3f5166dfc",
+    "url": "/chat337v2/static/js/2.17ab8799.chunk.js"
+  },
+  {
+    "revision": "e293e9d779fa2b04407c6b7d804158ee",
+    "url": "/chat337v2/static/js/2.17ab8799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60142e6f9d74d00ddda7",
+    "url": "/chat337v2/static/js/main.09f0d388.chunk.js"
+  },
+  {
+    "revision": "bfc24aee5e5e0a2d02e4",
+    "url": "/chat337v2/static/js/runtime-main.1d82aa11.js"
+  },
+  {
+    "revision": "cbd4e701269338259ee0b39a0b768167",
+    "url": "/chat337v2/static/media/RobotoCondensed-Bold.cbd4e701.ttf"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/chat337v2/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  },
+  {
+    "revision": "d5746e3b20453d6cee99ab27a0bdf949",
+    "url": "/chat337v2/static/media/Staatliches-Regular.d5746e3b.ttf"
+  }
+]);
